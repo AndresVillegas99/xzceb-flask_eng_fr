546c78f37dc96b626b381cadf5f9363602e64904
AndresVillegas99 <villegasa25@gmail.com>
2021-10-30 00:13:42 +0000

    changes

--- final_project/server.py
+++ final_project/server.py
@@ -20,8 +20,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    app.debug = True
-    app.run()
+    f = open("index.html", "w")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
